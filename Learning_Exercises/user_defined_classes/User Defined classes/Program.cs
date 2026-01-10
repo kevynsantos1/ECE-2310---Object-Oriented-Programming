b0b7bd84c0a0e7681d27c5a3b044ef04db9baa3c
kevynsantos1 <kevynsantos1804@gmail.com>
2026-01-10 14:12:35 -0800

    Add files via upload

---
+++ Learning_Exercises/user_defined_classes/User Defined classes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Defined_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate objects of Student Class
+            Student stud01 = new Student();
+            Student stud02 = new Student();
+            Student stud03 = new Student();
+
+            stud01.ChangeData("12345", "Kev", 4.4);
+
+            //use objects to call member functions
+            Console.WriteLine("----Student #1----");
+            stud01.DispInfo();
+            Console.WriteLine("----Student #2----");
+            stud02.DispInfo();
+            Console.WriteLine("----Student #3----");
+            stud03.DispInfo();
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Learning_Exercises/user_defined_classes/User Defined classes/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Defined_classes
+{
+    class Student //class definition
+    {
+        //attributes-data members
+        private string broncoID;
+        private string name;
+        private double gpa;
+        //behaviors-member functions/methods
+        public void Study()
+        {
+            Console.WriteLine("The student is studying.");
+        }
+        public void DispInfo()
+        {
+            Console.WriteLine("Bronco ID: {0}   Name: {1}   GPA: {2}", broncoID, name, gpa);
+        }
+
+        public void ChangeData(string ide, string namee, double gpae)
+        {
+            name = namee;
+            broncoID = ide;
+            gpa = gpae;
+        }
+    }
+}
